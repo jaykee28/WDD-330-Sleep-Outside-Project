c7def68e8b11210f7f8bbd0d4b95cc6beed1eb60
jaykee28 <jmufetu@gmail.com>
2026-03-18 00:14:29 +0200

    Fix cart functionality: add single product per click

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,21 @@
-import { setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
-// add to cart button event handler
+
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// add listener to all Add to Cart buttons
+const addToCartButtons = document.querySelectorAll("[data-add-to-cart]");
+addToCartButtons.forEach((button) => {
+  button.addEventListener("click", addToCartHandler);
+});
